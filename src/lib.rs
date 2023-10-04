630273ef7121fe7c0fc838dda14d1323cb2da9d1
Thomas Versteeg <t@versteeg.email>
2023-10-04 21:02:39 +0200

    feat: assets features

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,21 @@
 //! ### `default-font`
 //!
 //! Implements [`Default`] for [`font::Font`] with a font that's embedded into memory.
+//!
+//! ### `assets` (default)
+//!
+//! Allow loading of external assets.
+//! All assets should reside in a `assets/` folder in the root directory of the project.
+//!
+//! ### `hot-reloading-assets` (default)
+//!
+//! Hot-reload assets from disk when they are saved.
+//! Has no effect on the web target.
+//!
+//! ### `embedded-assets` (default on web)
+//!
+//! Bake _all_ assets in the `assets/` folder in the binary.
+//! When creating a release binary this feature flag should be enabled.
 
 #[cfg(feature = "window")]
 pub mod window;
@@ -22,6 +37,9 @@ pub use window::window;
 
 #[cfg(feature = "font")]
 pub mod font;
+
+#[cfg(feature = "assets")]
+pub mod assets;
 
 /// Re-export vek types.
 pub use vek;
