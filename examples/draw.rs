d435ffbc547473026016abd4f8f97c14a711c263
Thomas Versteeg <t@versteeg.email>
2024-02-19 03:22:55 +0700

    feat(canvas): add `draw_quad` and `draw_triangle` to `Canvas`

---
+++ examples/draw.rs
@@ -0,0 +1,65 @@
+use pixel_game_lib::{
+    vek::Extent2,
+    window::{KeyCode, WindowConfig},
+};
+use vek::Vec2;
+
+/// Open an empty window.
+fn main() {
+    // Window configuration with huge pixels
+    let window_config = WindowConfig {
+        buffer_size: Extent2::new(64, 64),
+        scaling: 8,
+        ..Default::default()
+    };
+
+    // Open the window and start the game-loop
+    pixel_game_lib::window(
+        // Keep track of the mouse as our "game state"
+        Vec2::zero(),
+        window_config.clone(),
+        // Update loop exposing input events we can handle, this is where you would handle the game logic
+        |state, input, mouse, _dt| {
+            // Set the mouse position as the game state
+            if let Some(mouse) = mouse {
+                *state = mouse;
+            }
+
+            // Exit when escape is pressed
+            input.key_pressed(KeyCode::Escape)
+        },
+        // Render loop exposing the pixel buffer we can mutate
+        move |mouse, canvas, _dt| {
+            // Reset the canvas with a white color
+            canvas.fill(0xFFFFFFFF);
+
+            // Draw a light green blue triangle with one corner being snapped to the mouse
+            canvas.draw_triangle(
+                [Vec2::new(45.0, 5.0), Vec2::new(60.0, 8.0), mouse.as_()],
+                0xFF99FF99,
+            );
+
+            // Draw a light blue quadrilateral with one corner being snapped to the mouse
+            canvas.draw_quad(
+                [
+                    Vec2::new(5.0, 5.0),
+                    Vec2::new(20.0, 8.0),
+                    Vec2::new(8.0, 30.0),
+                    mouse.as_(),
+                ],
+                0xFF9999FF,
+            );
+
+            // Draw a black line from the center of the canvas to our mouse
+            canvas.draw_line(
+                (window_config.buffer_size.as_() / 2.0).into(),
+                mouse.as_(),
+                0xFF000000,
+            );
+
+            // Draw a red pixel under the mouse
+            canvas.set_pixel(mouse.as_(), 0xFFFF0000);
+        },
+    )
+    .expect("Error opening window");
+}
--- src/canvas.rs
+++ src/canvas.rs
@@ -1,7 +1,9 @@
 //! Wrapper around a pixel buffer.
 
+use std::cmp::Ordering;
+
 use line_drawing::Bresenham;
-use vek::{Extent2, Vec2};
+use vek::{Extent2, LineSegment2, Vec2};
 
 /// Simple wrapper around a pixel buffer that can be passed around to rendering calls.
 pub struct Canvas<'a> {
@@ -45,6 +47,92 @@ impl<'a> Canvas<'a> {
         }
     }
 
+    /// Fill a triangle.
+    ///
+    /// Based on: https://joshbeam.com/articles/triangle_rasterization/
+    pub fn draw_triangle(&mut self, corners: [Vec2<f64>; 3], color: u32) {
+        // Create the 3 edges from the triangle
+        let edges = [
+            Edge::new(corners[0], corners[1]),
+            Edge::new(corners[1], corners[2]),
+            Edge::new(corners[2], corners[0]),
+        ];
+
+        // Find the longest edge
+        let Some((longest_edge_index, longest_edge)) =
+            edges.iter().enumerate().max_by(|(_, edge1), (_, edge2)| {
+                edge1
+                    .y_length()
+                    .partial_cmp(&edge2.y_length())
+                    .unwrap_or(Ordering::Equal)
+            })
+        else {
+            // Something weird happened, just don't do anything in that case
+            return;
+        };
+
+        // Find the other two edges
+        let short_edge1 = &edges[(longest_edge_index + 1) % 3];
+        let short_edge2 = &edges[(longest_edge_index + 2) % 3];
+
+        // Draw the spans between both edges
+        self.draw_span_between_edges(longest_edge, short_edge1, color);
+        self.draw_span_between_edges(longest_edge, short_edge2, color);
+    }
+
+    /// Fill a polygon with 4 corners.
+    ///
+    ///
+    /// If any of the 4 points falls inside the triangle of the other three only a single triangle will be drawn.
+    ///
+    /// Based on: https://stackoverflow.com/a/2122620
+    pub fn draw_quad(&mut self, corners: [Vec2<f64>; 4], color: u32) {
+        fn signed_area(a: Vec2<f64>, b: Vec2<f64>, c: Vec2<f64>) -> f64 {
+            (a.y - b.y) * c.x + (b.x - a.x) * c.y + (a.x * b.y - b.x * a.y)
+        }
+
+        let abc_is_clockwise = signed_area(corners[0], corners[1], corners[2]) > 0.0;
+
+        let abd_is_clockwise = signed_area(corners[0], corners[1], corners[3]) > 0.0;
+        let bcd_is_clockwise = signed_area(corners[1], corners[2], corners[3]) > 0.0;
+        let cad_is_clockwise = signed_area(corners[2], corners[0], corners[3]) > 0.0;
+
+        // Match by checking the other triangle signs against ABC
+        match (
+            abd_is_clockwise == abc_is_clockwise,
+            bcd_is_clockwise == abc_is_clockwise,
+            cad_is_clockwise == abc_is_clockwise,
+        ) {
+            // ABC ABD
+            (false, true, true) => {
+                self.draw_triangle([corners[0], corners[1], corners[2]], color);
+                self.draw_triangle([corners[0], corners[1], corners[3]], color);
+            }
+            // ABC BCD
+            (true, false, true) => {
+                self.draw_triangle([corners[0], corners[1], corners[2]], color);
+                self.draw_triangle([corners[1], corners[2], corners[3]], color);
+            }
+            // ABC CAD
+            (true, true, false) => {
+                self.draw_triangle([corners[0], corners[1], corners[2]], color);
+                self.draw_triangle([corners[2], corners[0], corners[3]], color);
+            }
+
+            // D is inside ABC
+            (true, true, true) => self.draw_triangle([corners[0], corners[1], corners[2]], color),
+            // C is inside ABD
+            (true, false, false) => self.draw_triangle([corners[0], corners[1], corners[3]], color),
+            // B is inside CAD
+            (false, false, true) => self.draw_triangle([corners[0], corners[2], corners[3]], color),
+            // A is inside BCD
+            (false, true, false) => self.draw_triangle([corners[1], corners[2], corners[3]], color),
+
+            // Shouldn't happen
+            _ => (),
+        }
+    }
+
     /// Fill the canvas with a single color.
     #[inline]
     pub fn fill(&mut self, color: u32) {
@@ -73,5 +161,90 @@ impl<'a> Canvas<'a> {
     #[inline]
     pub fn size(&self) -> Extent2<usize> {
         self.size
+    }
+}
+
+/// Drawing helpers.
+impl<'a> Canvas<'a> {
+    /// Draw a span between two edges, this fills everything in between.
+    fn draw_span_between_edges(&mut self, long: &Edge, short: &Edge, color: u32) {
+        let long_y_length = long.y_length();
+        // Ignore horizontal edges
+        if long_y_length.abs() < 0.5 {
+            return;
+        }
+
+        let short_y_length = short.y_length();
+        // Ignore horizontal edges
+        if short_y_length.abs() < 0.5 {
+            return;
+        }
+
+        let long_x_diff = long.x_diff();
+        let short_x_diff = short.x_diff();
+
+        // Calculate interpolation factors
+        let mut long_factor = (short.0.start.y - long.0.start.y) / long_y_length;
+        let long_factor_step = long_y_length.recip();
+        let mut short_factor = 0.0;
+        let short_factor_step = short_y_length.recip();
+
+        // Clamp to the canvas
+        let start_y = (short.0.start.y.floor().max(0.0) as usize).min(self.size.h);
+        let end_y = (short.0.end.y.ceil().max(0.0) as usize).min(self.size.h);
+
+        for y in start_y..end_y {
+            // Calculate the X based on the interpolation by Y
+            let long_x = long.0.start.x + long_x_diff * long_factor;
+            let short_x = short.0.start.x + short_x_diff * short_factor;
+
+            let (start_x, end_x) = if long_x < short_x {
+                (long_x, short_x)
+            } else {
+                (short_x, long_x)
+            };
+
+            // Clamp to the buffer
+            let start_x = (start_x.floor().max(0.0) as usize).min(self.size.w);
+            let end_x = (end_x.ceil().max(0.0) as usize).min(self.size.w);
+
+            // Draw the pixels
+            let y_index = y * self.size.w;
+            self.buffer[(y_index + start_x)..(y_index + end_x)].fill(color);
+
+            // Increase interpolation factors
+            long_factor += long_factor_step;
+            short_factor += short_factor_step;
+        }
+    }
+}
+
+/// Edge helper wrapper for line segments.
+struct Edge(LineSegment2<f64>);
+
+impl Edge {
+    /// Create an edge, sorting the Y coordinates.
+    pub fn new(point1: Vec2<f64>, point2: Vec2<f64>) -> Self {
+        if point1.y < point2.y {
+            Self(LineSegment2 {
+                start: point1,
+                end: point2,
+            })
+        } else {
+            Self(LineSegment2 {
+                start: point2,
+                end: point1,
+            })
+        }
+    }
+
+    /// Difference across the X axis, not length because it can be negative.
+    fn x_diff(&self) -> f64 {
+        self.0.end.x - self.0.start.x
+    }
+
+    /// Length across the Y axis.
+    fn y_length(&self) -> f64 {
+        self.0.end.y - self.0.start.y
     }
 }
