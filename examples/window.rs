4858140b93bc67e4d903c3a6342e0fb26159ee4f
Thomas Versteeg <t@versteeg.email>
2024-03-03 14:18:54 +0100

    feat(state): create a `PixelGame` trait which simplifies setting up a new game with a window

--- examples/window.rs
+++ examples/window.rs
@@ -1,7 +1,11 @@
 use pixel_game_lib::{
+    canvas::Canvas,
     vek::Extent2,
     window::{KeyCode, WindowConfig},
+    PixelGame,
 };
+use vek::Vec2;
+use winit_input_helper::WinitInputHelper;
 
 /// Define a game state with a simple counter.
 struct GameState {
@@ -9,6 +13,37 @@ struct GameState {
     ///
     /// For each number a pixel is drawn.
     pub pixels_to_draw: usize,
+}
+
+impl PixelGame for GameState {
+    // Update loop exposing input events we can handle, this is where you would handle the game logic
+    fn update(
+        &mut self,
+        input: &WinitInputHelper,
+        _mouse_pos: Option<Vec2<usize>>,
+        _dt: f32,
+    ) -> bool {
+        // Increment when mouse is clicked
+        if input.mouse_held(0) {
+            self.pixels_to_draw += 1;
+        }
+
+        // Exit when escape is pressed
+        input.key_pressed(KeyCode::Escape)
+    }
+
+    // Render loop exposing the pixel buffer we can mutate
+    fn render(&mut self, canvas: &mut Canvas<'_>) {
+        // Fill the window with a background color, if we don't fill it the pixels of the last frame will be drawn again
+        canvas.fill(0xFFFFFFFF);
+
+        // Ensure that we don't draw pixels outside of the canvas
+        let max_pixels_to_draw = canvas.size().product();
+        let pixels_to_draw = self.pixels_to_draw.min(max_pixels_to_draw);
+
+        // Draw a red color for each pixel
+        canvas.raw_buffer()[0..pixels_to_draw].fill(0xFFFF0000);
+    }
 }
 
 /// Open an empty window.
@@ -23,29 +58,5 @@ fn main() {
         ..Default::default()
     };
 
-    // Open the window and start the game-loop
-    pixel_game_lib::window(
-        state,
-        window_config.clone(),
-        // Update loop exposing input events we can handle, this is where you would handle the game logic
-        |state, input, _mouse, _dt| {
-            // Increment when mouse is clicked
-            if input.mouse_held(0) {
-                state.pixels_to_draw += 1;
-            }
-
-            // Exit when escape is pressed
-            input.key_pressed(KeyCode::Escape)
-        },
-        // Render loop exposing the pixel buffer we can mutate
-        move |state, canvas, _dt| {
-            // Ensure that we don't draw pixels outside of the canvas
-            let max_pixels_to_draw = window_config.buffer_size.product();
-            let pixels_to_draw = state.pixels_to_draw.min(max_pixels_to_draw);
-
-            // Draw a red color for each pixel
-            canvas.raw_buffer()[0..pixels_to_draw].fill(0xFFFF0000);
-        },
-    )
-    .expect("Error opening window");
+    state.run(window_config).expect("Error running game");
 }
