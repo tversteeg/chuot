1aea90de2ce8a79ec1ac770ed555940a0871ca1f
tversteeg <tversteeg@users.noreply.github.com>
2024-03-04 21:49:28 +0100

    fix(ci): install audio dependency in CI

--- src/lib.rs
+++ src/lib.rs
@@ -12,41 +12,41 @@
 //!
 //! # Feature Flags
 //!
-//! ### `default-font`
+//! ## `default-font`
 //!
 //! Implements [`Default`] for [`font::Font`] with a font that's embedded into memory.
 //!
-//! ### `default-gui`
+//! ## `default-gui`
 //!
 //! Implements [`Default`] for different GUI elements with a images embedded into memory.
 //!
-//! ### `hot-reloading-assets` (default)
+//! ## `hot-reloading-assets` (default)
 //!
 //! Hot-reload assets from disk when they are saved.
 //! Has no effect on the web target.
 //!
-//! ### `embedded-assets` (default on web)
+//! ## `embedded-assets` (default on web)
 //!
 //! Bake _all_ assets in the `assets/` folder in the binary.
 //! When creating a release binary this feature flag should be enabled.
 //!
-//! ### `physics`
+//! ## `physics`
 //!
 //! Enable the 2D XPBD-based physics engine.
 //!
-//! ### `dialogue`
+//! ## `dialogue`
 //!
 //! A thin wrapper around [Yarn Spinner](https://www.yarnspinner.dev/).
 //! Allows creating hot-reloadable dialogue systems.
 //!
-//! ### `audio`
+//! ## `audio`
 //!
 //! A thin wrapper around [Kira](https://docs.rs/kira/latest/kira/).
 //! Play sounds and music files which can be hot-reloadable using assets.
 //!
 //! To keep the binary and compile-times small only `.ogg` audio files are supported.
 //!
-//! #### Requirements
+//! ### Requirements
 //!
 //! On Linux you need to install `asound2-dev`:
 //!
