d3416400561524d9620f9f58321b04c3d037e9e3
Thomas Versteeg <t@versteeg.email>
2024-04-27 20:33:58 +0200

    fix(features)!: disable `hot-reload-assets` and `in-game-profiler` as default features

--- src/graphics/state.rs
+++ src/graphics/state.rs
@@ -201,24 +201,32 @@ impl<'window> MainRenderState<'window> {
 
     /// Upload everything that gets added at another point in time where the graphics state wasn't available.
     pub(crate) fn upload(&mut self, images: Vec<(AtlasRef, PngReader)>) {
-        images.into_iter().for_each(|(atlas_id, mut image_reader)| {
-            // Read the PNG
-            let mut buf = vec![0; image_reader.output_buffer_size()];
-            let info = image_reader
-                .next_frame(&mut buf)
-                .expect("Error reading image");
+        {
+            profiling::scope!("Upload images");
 
-            // Upload the PNG
-            let uploaded_id = self.atlas.add_texture(
-                Size2::new(info.width, info.height),
-                bytemuck::cast_slice(&buf),
-                &self.gpu.queue,
-            );
-            assert_eq!(uploaded_id, atlas_id);
-        });
+            images.into_iter().for_each(|(atlas_id, mut image_reader)| {
+                // Read the PNG
+                let mut buf = vec![0; image_reader.output_buffer_size()];
+                let info = image_reader
+                    .next_frame(&mut buf)
+                    .expect("Error reading image");
 
-        // Upload uniforsm
-        self.atlas.rects.upload(&self.gpu.queue);
+                // Upload the PNG
+                let uploaded_id = self.atlas.add_texture(
+                    Size2::new(info.width, info.height),
+                    bytemuck::cast_slice(&buf),
+                    &self.gpu.queue,
+                );
+                assert_eq!(uploaded_id, atlas_id);
+            });
+        }
+
+        {
+            profiling::scope!("Upload uniforms");
+
+            // Upload uniforsm
+            self.atlas.rects.upload(&self.gpu.queue);
+        }
     }
 
     /// Resize the surface.
