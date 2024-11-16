39ce272f5ba2dc835a59cbda536a20209070d03b
Thomas Versteeg <t@versteeg.email>
2024-11-16 16:59:02 +0700

    feat(sprite): add `Pivot::Fraction` to the enum

--- src/pivot.rs
+++ src/pivot.rs
@@ -7,17 +7,45 @@ use nanoserde::DeRon;
 #[non_exhaustive]
 pub enum Pivot {
     /// Middle of the sprite will be rendered at `(0, 0)`.
+    ///
+    /// Equivalent to:
+    ///
+    /// ```
+    /// Pivot::Fraction { x: 0.5, y: 0.5 }
+    /// ```
     Middle,
     /// Horizontal middle and vertical top will be rendered at `(0, 0)`.
+    ///
+    /// Equivalent to:
+    ///
+    /// ```
+    /// Pivot::Fraction { x: 0.5, y: 0.0 }
+    /// ```
     MiddleTop,
     /// Left top of the sprite will be rendered at `(0, 0)`.
+    ///
+    /// Equivalent to:
+    ///
+    /// ```
+    /// Pivot::Fraction { x: 0.0, y: 0.0 }
+    /// ```
     #[default]
     LeftTop,
-    /// Sprite will be offset with the custom coordinates counting from the left top.
+    /// Sprite will be offset with the absolute custom coordinates counting from the left top.
     Custom {
-        /// X offset from the left.
+        /// Absolute X offset as pixels from the left.
         x: f32,
-        /// Y offset from the top.
+        /// Absolute Y offset as pixels from the top.
+        y: f32,
+    },
+    /// Sprite will be offset with the relative coordinates as a fraction counting from the left top.
+    ///
+    /// `(0.0, 0.0)` means the top left.
+    /// `(1.0, 1.0)` means the bottom left.
+    Fraction {
+        /// Relative X offset as a fraction from the left.
+        x: f32,
+        /// Relative Y offset as a fraction from the top.
         y: f32,
     },
 }
@@ -31,6 +59,7 @@ impl Pivot {
             Self::MiddleTop => (-sprite_width / 2.0, 0.0),
             Self::LeftTop => (0.0, 0.0),
             Self::Custom { x, y } => (-x, -y),
+            Self::Fraction { x, y } => (-x * sprite_width, -y * sprite_height),
         }
     }
 }
